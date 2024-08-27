0134d364abcde947dc5dc7cc98a67e3bdea9a1be
Eloi Torrents <eloitor@disroot.org>
2024-08-27 08:30:51 +0200

    Get the port with smallest fd

--- get_port.py
+++ get_port.py
@@ -1,26 +1,35 @@
 import psutil
-from typing import List, Optional
+from typing import Optional
 
-def get_anytype_ports() -> List[int]:
-    ports = []
+def read_fd_max() -> int:
+    try:
+        with open('/proc/sys/fs/file-max', 'r') as file:
+            pid_max = file.read().strip()
+        return int(pid_max)
+    except FileNotFoundError:
+        raise RuntimeError("The file /proc/sys/kernel/pid_max does not exist.")
+    except PermissionError:
+        raise RuntimeError("Permission denied when trying to read /proc/sys/kernel/pid_max.")
+    except ValueError:
+        raise RuntimeError("Unexpected content in /proc/sys/kernel/pid_max; could not convert to an integer.")
+
+def get_anytype_port() -> int:
+    first_port_fd = read_fd_max()
+    first_port = None
     try:
         for conn in psutil.net_connections(kind='inet'):
-            if conn.status == psutil.CONN_LISTEN and 'anytype' in psutil.Process(conn.pid).name().lower():
-                ports.append(conn.laddr.port)
-        return ports  # Return the list of ports
+            if conn.fd < first_port_fd and  conn.status == psutil.CONN_LISTEN and  'anytype' in psutil.Process(conn.pid).name().lower():
+                first_port_fd = conn.fd
+                first_port = conn.laddr.port
+        return first_port
     except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess) as e:
         raise RuntimeError(f"An error occurred while retrieving ports: {str(e)}") from e
 
-def get_anytype_port() -> Optional[int]:
-    ports = get_anytype_ports()  # Reuse the function to get all ports
-    return min(ports) if ports else None 
-
-
 if __name__ == "__main__":
     try:
-        ports = get_anytype_ports()
+        ports = get_anytype_port()
         if ports:
-            print(f"Anytype is listening on ports: {ports}")
+            print(f"Anytype is listening on port: {ports}")
         else:
             print("No Anytype port found.")
     except RuntimeError as e:
